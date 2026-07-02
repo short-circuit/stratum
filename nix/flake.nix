086cbd31bee2b6c59d5d56ef97f85f59eae173c6
Davide Del Popolo <davidedp91@gmail.com>
2026-07-02 11:40:46 +0200

    v0.4.2

--- nix/flake.nix
+++ nix/flake.nix
@@ -96,7 +96,7 @@
 
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "stratum";
-          version = "0.4.1";
+          version = "0.4.2";
           src = ../.;
 
           cargoLock.lockFile = ../Cargo.lock;
